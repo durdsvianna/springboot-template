8e1dc679ed6448dd9dbd47a6a6d5fa6926d4af61
Daniel Vianna <durds.vianna@gmail.com>
2025-03-20 20:47:44 -0300

    Add MongoDB TestContainer configuration

--- src/test/java/com/example/customerservice/config/MongoTestContainer.java
+++ src/test/java/com/example/customerservice/config/MongoTestContainer.java
@@ -2,20 +2,29 @@ package com.example.customerservice.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration
 public class MongoTestContainer {
-    
-    private static final String MONGODB_VERSION = "6.0.6";
-    
+
+    private static final MongoDBContainer mongoDBContainer;
+
+    static {
+        mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:5.0.15"));
+        mongoDBContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("embedded.mongodb.host", () -> "localhost");
+        registry.add("embedded.mongodb.port", mongoDBContainer::getFirstMappedPort);
+    }
+
     @Bean
     public MongoDBContainer mongoDBContainer() {
-        MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:" + MONGODB_VERSION));
-        mongoDBContainer.start();
-        
-        System.setProperty("spring.data.mongodb.uri", mongoDBContainer.getReplicaSetUrl());
         return mongoDBContainer;
     }
-} 
+}
