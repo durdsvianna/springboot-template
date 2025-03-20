2b9907a8afc2bbc1feef1327174387fed74f8317
Daniel Vianna <durds.vianna@gmail.com>
2025-03-20 20:48:10 -0300

    Add Address domain model

--- src/main/java/com/example/customerservice/model/Address.java
+++ src/main/java/com/example/customerservice/model/Address.java
@@ -1,42 +1,53 @@
 package com.example.customerservice.model;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "addresses")
+@CompoundIndexes({
+    @CompoundIndex(name = "customer_city_idx", def = "{'customerId': 1, 'city': 1}")
+})
 public class Address {
-    
+
     @Id
     private String id;
     
-    @NotBlank(message = "Street is required")
+    private String customerId;
+    
     private String street;
     
-    @NotBlank(message = "City is required")
     private String city;
     
-    @NotBlank(message = "State is required")
     private String state;
     
-    @NotBlank(message = "Zip code is required")
-    @Pattern(regexp = "^\\d{5}(-\\d{4})?$", message = "Zip code must be in format 12345 or 12345-6789")
     private String zipCode;
     
     private String country;
     
-    private Boolean isDefault;
+    @Builder.Default
+    private boolean isDefault = false;
     
-    // Reference to the customer this address belongs to
-    private String customerId;
-} 
+    @Version
+    private Long version;
+    
+    @Builder.Default
+    private LocalDateTime createdDate = LocalDateTime.now();
+    
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+}
