6ba16ab5af865305e4d1b5b3a98e1104c09aacb6
Daniel Vianna <durds.vianna@gmail.com>
2025-03-20 20:47:33 -0300

    Add main application class

--- src/main/java/com/example/customerservice/CustomerServiceApplication.java
+++ src/main/java/com/example/customerservice/CustomerServiceApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.Contact;
 
 @SpringBootApplication
 @OpenAPIDefinition(
     info = @Info(
         title = "Customer Service API",
         version = "1.0",
-        description = "API for managing customers and their addresses"
+        description = "API for managing customers and their addresses",
+        contact = @Contact(name = "Development Team", email = "dev@example.com")
     )
 )
 public class CustomerServiceApplication {
@@ -18,4 +20,4 @@ public class CustomerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
-} 
+}
